6c437835332739526fcf99a8e11d5359ec8367a3
Dawid Stasiak <dawid.stasiak17@gmail.com>
2021-02-06 23:30:53 +0100

    All of it
    I don't wanna change it, it's my pure childhood  of programming

---
+++ cmake-build-debug/CMakeFiles/checkers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkers.dir/src/checkers.cpp.o"
+  "checkers"
+  "checkers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checkers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
